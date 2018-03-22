23445ecff2c2969f8c935a961d20f8c3b4aebdfa
张强 <qiangzhang@huangbaoche.com>
2018-03-22 17:50:26 +0800

    添加libary，为实现jitpack与github结合，完成其他工程引用github中此widgt库

---
+++ mylibrary/src/main/java/xhxny/com/mylibrary/Uitils.java
@@ -0,0 +1,11 @@
+package xhxny.com.mylibrary;
+
+/**
+ * Created by zhangqiang on 2018/3/22.
+ */
+
+public class Uitils {
+    public static String getString(){
+        return "im listron";
+    }
+}
